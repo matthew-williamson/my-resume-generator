66c701e1d77b9528b9cf8a8fcc8d444bade4049c
Matthew Williamson <matt.williamson@imaginelearning.com>
2024-02-29 21:08:03 -0700

    Fix window bugs

--- app/ui/hooks/useWindowSize.ts
+++ app/ui/hooks/useWindowSize.ts
@@ -5,8 +5,8 @@ const useWindowSize = () => {
     width: number;
     height: number;
   }>({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width:  window?.innerWidth || 0,
+    height: window?.innerHeight || 0,
   });
 
   useEffect(() => {
